9ee8b6a4804e1e922c9b1010bb63a536cee7048a
holeczpeter <holecz.peter85@gmail.com>
2023-08-31 12:43:40 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
+#!/usr/bin/env sh
+
 set -e
+
 npm run build
+
 cd dist
+
 git init
 git add -A
 git commit -m 'New Deployment'
+# git push -f git@github.com:holeczpeter/test.git master:gh-pages
 git push -f https://github.com/holeczpeter/holeczpeter.git master:gh-pages
 cd -
