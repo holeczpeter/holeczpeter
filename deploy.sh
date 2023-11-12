1b721f090aa1c33b6ab1169707813d2e63fb81a7
holeczpeter <holecz.peter85@gmail.com>
2023-11-12 16:57:20 +0100

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,20 @@
 
 set -e
 
+#DEPLOYMENT_MESSAGE
 DEPLOYMENT_MESSAGE='New Build'
 
+#build
 npm run build
+
+#deploy.yml 
 mkdir -p dist/.github/workflows
 cp .github/workflows/deploy.yml dist/.github/workflows/deploy.yml
 
-
+#commit
 cd dist
 git init
 git add -A
 git commit -m "$DEPLOYMENT_MESSAGE"
-# git push -f git@github.com:holeczpeter/test.git master:gh-pages
 git push -f https://github.com/holeczpeter/holeczpeter.git master:gh-pages
 cd -
