9e03c795d9105b1d601a2bba767404e5ee10e37d
holeczpeter <holecz.peter85@gmail.com>
2023-11-12 16:01:05 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 npm run build
-
+cp .github/workflows/deploy.yml dist/.github/workflows/
 cd dist
 
 git init
@@ -12,29 +12,3 @@ git commit -m 'New Deployment'
 # git push -f git@github.com:holeczpeter/test.git master:gh-pages
 git push -f https://github.com/holeczpeter/holeczpeter.git master:gh-pages
 cd -
-
-if [ ! -f ../.github/workflows/deploy.yml ]; then
-  # If not, create the deploy.yml file
-  echo "name: Deploy to GitHub Pages
-
-on:
-  push:
-    branches:
-      - master  # A fő ág neve, cseréld le az aktuális ág nevére
-
-jobs:
-  deploy:
-    runs-on: ubuntu-latest
-
-    steps:
-    - name: Checkout repository
-      uses: actions/checkout@v2
-
-    - name: FTP Deploy
-      uses: SamKirkland/FTP-Deploy-Action@4.0.0
-      with:
-        server: \${{ secrets.FTP_SERVER }}
-        username: \${{ secrets.FTP_USERNAME }}
-        password: \${{ secrets.FTP_PASSWORD }}
-        local-dir: .  # A repository tartalmának helye, itt az aktuális könyvtár
-        server-dir: /public_html  # Az FTP-szerveren a célmappa" > ../.github/workflows/deploy.yml
